67a0faf71fd77fdaa174d338f3c0b6ee350c8162
EimantasLedinauskas <eimantasl@gmail.com>
2019-04-03 10:27:21 +0300

    first commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='drone_det_tools',
+      version='0.1',
+      author='Eimantas',
+      license='None',
+      packages=['drone_det_tools'],
+      zip_safe=False)
